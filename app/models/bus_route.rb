116ad1cc14d2b08d5aa8b417db4730273dee59c8
Raul <raulpopadineti@gmail.com>
2012-07-12 00:32:21 +0300

    rails app init + mongoid

---
+++ app/models/bus_route.rb
@@ -0,0 +1,7 @@
+class BusRoute
+  include Mongoid::Document
+
+
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TranzitmRor::Application.config.secret_token = '54cc5504a16fbc420094cfd155724be42f39693ee1605b4c7bb13d7f506501ddc9d1a22e5a3d40be156e037cb3f023b159718597b10b4e9e0ea41f7726505585'
